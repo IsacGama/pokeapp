deba2f77c7897478c7a36addcd9a4e1c94b34961
Isac Gama <87917139+IsacGama@users.noreply.github.com>
2025-06-17 20:42:08 -0300

    chore: recriação do projeto com estrutura Standalone para corrigir problemas de build

--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -1,21 +1,9 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PokemonService {
 
-  private baseURL = 'https://pokeapi.co/api/v2/pokemon';
-
-  constructor(private http: HttpClient) {}
-
-  getPokemons(offset: number, limit: number): Observable<any> {
-    return this.http.get(`${this.baseURL}?offset=${offset}&limit=${limit}`);
-  }
-
-  getPokemonDetails(name: string): Observable<any> {
-    return this.http.get(`${this.baseURL}/${name}`);
-  }
+  constructor() { }
 }
