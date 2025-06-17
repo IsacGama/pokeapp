82f1f4f87ff729a9bad48d81d1f8a25ac7274907
Isac Gama <87917139+IsacGama@users.noreply.github.com>
2025-06-17 20:47:08 -0300

    feat: service pokemon.service and refactor home page

--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PokemonService {
+  private apiUrl = 'https://pokeapi.co/api/v2/pokemon';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getPokemonList(offset: number = 0, limit: number = 20): Observable<any> {
+    return this.http.get(`${this.apiUrl}?offset=${offset}&limit=${limit}`);
+  }
+
+  getPokemonDetails(name: string): Observable<any> {
+    return this.http.get(`${this.apiUrl}/${name}`);
+  }
 }
