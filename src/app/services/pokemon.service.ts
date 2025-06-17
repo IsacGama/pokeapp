4773dc3925975afcc3427512f28bcd1fc4dde562
Isac Gama <87917139+IsacGama@users.noreply.github.com>
2025-06-17 14:01:35 -0300

    feat: criação do serviço PokemonService para consumo da PokeAPI

--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PokemonService {
 
-  constructor() { }
+  private baseURL = 'https://pokeapi.co/api/v2/pokemon';
+
+  constructor(private http: HttpClient) {}
+
+  getPokemons(offset: number, limit: number): Observable<any> {
+    return this.http.get(`${this.baseURL}?offset=${offset}&limit=${limit}`);
+  }
+
+  getPokemonDetails(name: string): Observable<any> {
+    return this.http.get(`${this.baseURL}/${name}`);
+  }
 }
