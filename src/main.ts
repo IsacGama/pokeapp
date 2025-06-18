4ae38d1bb694287fd5d47a419be2075988e6719c
Isac Gama <87917139+IsacGama@users.noreply.github.com>
2025-06-18 15:47:19 -0300

    fix angular.json and main.ts for better performance

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
-import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
+import {  provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
+import {  provideIonicAngular } from '@ionic/angular/standalone';
 import { provideHttpClient } from '@angular/common/http';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
@@ -9,7 +10,7 @@ import { AppComponent } from './app/app.component';
 bootstrapApplication(AppComponent, {
   providers: [
     provideHttpClient(),
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    { provide: LocationStrategy, useClass: HashLocationStrategy },
     provideIonicAngular(),
     provideRouter(routes, withPreloading(PreloadAllModules)),
   ],
